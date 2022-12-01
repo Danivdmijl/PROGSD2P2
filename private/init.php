69c7b1b2d58a62089e07b68f93515a66f28fd7bc
Danivdmijl <32570@ma-web.nl>
2022-12-01 12:05:44 +0100

    Opdracht 2 week 1

--- private/init.php
+++ private/init.php
@@ -2,4 +2,17 @@
 include "/Downloads/xamp/htdocs/PROGSD2P2/private";
 include "/Downloads/xamp/htdocs/PROGSD2P2/private/shared";
 include "/Downloads/xamp/htdocs/PROGSD2P2";
+
+$root = substr($_SERVER['SCRIPT_NAME'],
+    0,
+    strpos($_SERVER['SCRIPT_NAME'], '/public') + 7);
+define("ROOT_URL", $root);
+
+function url_path($path) {
+    if($path[0] != '/') {
+        $path = "/" . $path;
+    }
+    return ROOT_URL . $path;
+}
+
 ?>
---
+++ public/contentbeheer/index.php
@@ -0,0 +1,29 @@
+<?php
+$url = "../../private/shared/header.php";
+include $url;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Startpagina</title>
+</head>
+<body>
+<header>
+  Welkom
+</header>
+
+<h1>
+  Standaard tekst
+</h1>
+
+<footer>
+
+</footer>
+  Privacy verklaring
+</body>
+<?php
+include "../../private/shared/footer.php";
+?>
+</html>
